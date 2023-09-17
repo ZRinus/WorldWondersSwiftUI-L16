7bc475fd52f9a6fb74cc2aacb54ad5e6d1acd68e
Rinat Zaripov <z_renat@inbox.ru>
2023-09-17 10:59:57 +0600

    Initial Commit

---
+++ WorldWondersSwiftUI/WorldWondersSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldWondersSwiftUIApp.swift
+//  WorldWondersSwiftUI
+//
+//  Created by Rinat Zaripov on 17.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct WorldWondersSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
